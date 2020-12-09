1082cade366dffc046d371b0175057c7cc9b869c
velopess2 <75450115+velopess2@users.noreply.github.com>
2020-12-09 14:18:17 -0300

    Formulario.teste2

---
+++ Cadastro/Cadastro/Cadastro/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Cadastro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Nome = new System.Windows.Forms.TextBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_sobrenome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Email = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_telefone = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_Endereco = new System.Windows.Forms.TextBox();
+            this.btn_pesquisar = new System.Windows.Forms.Button();
+            this.btn_Excluir = new System.Windows.Forms.Button();
+            this.btn_Editar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // txt_Nome
+            // 
+            this.txt_Nome.Location = new System.Drawing.Point(70, 99);
+            this.txt_Nome.Name = "txt_Nome";
+            this.txt_Nome.Size = new System.Drawing.Size(181, 20);
+            this.txt_Nome.TabIndex = 1;
+            this.txt_Nome.TextChanged += new System.EventHandler(this.txt_Nome_TextChanged);
+            // 
+            // btn_add
+            // 
+            this.btn_add.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_add.Location = new System.Drawing.Point(70, 294);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 2;
+            this.btn_add.Text = "Adicionar";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sobrenome";
+            // 
+            // txt_sobrenome
+            // 
+            this.txt_sobrenome.Location = new System.Drawing.Point(278, 99);
+            this.txt_sobrenome.Name = "txt_sobrenome";
+            this.txt_sobrenome.Size = new System.Drawing.Size(186, 20);
+            this.txt_sobrenome.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(198, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Email";
+            // 
+            // txt_Email
+            // 
+            this.txt_Email.Location = new System.Drawing.Point(192, 144);
+            this.txt_Email.Name = "txt_Email";
+            this.txt_Email.Size = new System.Drawing.Size(272, 20);
+            this.txt_Email.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Data Nascimento";
+            // 
+            // txt_dateTimePicker1
+            // 
+            this.txt_dateTimePicker1.CustomFormat = "";
+            this.txt_dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txt_dateTimePicker1.Location = new System.Drawing.Point(70, 144);
+            this.txt_dateTimePicker1.Name = "txt_dateTimePicker1";
+            this.txt_dateTimePicker1.Size = new System.Drawing.Size(100, 20);
+            this.txt_dateTimePicker1.TabIndex = 8;
+            this.txt_dateTimePicker1.Value = new System.DateTime(2020, 12, 8, 11, 35, 59, 0);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(70, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Telefone";
+            // 
+            // txt_telefone
+            // 
+            this.txt_telefone.Location = new System.Drawing.Point(70, 194);
+            this.txt_telefone.Mask = "(00)00000-0000";
+            this.txt_telefone.Name = "txt_telefone";
+            this.txt_telefone.Size = new System.Drawing.Size(100, 20);
+            this.txt_telefone.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(70, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Endereco";
+            // 
+            // txt_Endereco
+            // 
+            this.txt_Endereco.Location = new System.Drawing.Point(70, 245);
+            this.txt_Endereco.Name = "txt_Endereco";
+            this.txt_Endereco.Size = new System.Drawing.Size(394, 20);
+            this.txt_Endereco.TabIndex = 12;
+            // 
+            // btn_pesquisar
+            // 
+            this.btn_pesquisar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_pesquisar.Location = new System.Drawing.Point(155, 294);
+            this.btn_pesquisar.Name = "btn_pesquisar";
+            this.btn_pesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btn_pesquisar.TabIndex = 14;
+            this.btn_pesquisar.Text = "Pesquisar";
+            this.btn_pesquisar.UseVisualStyleBackColor = true;
+            this.btn_pesquisar.Click += new System.EventHandler(this.btn_pesquisar_Click);
+            // 
+            // btn_Excluir
+            // 
+            this.btn_Excluir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Excluir.Location = new System.Drawing.Point(245, 294);
+            this.btn_Excluir.Name = "btn_Excluir";
+            this.btn_Excluir.Size = new System.Drawing.Size(75, 23);
+            this.btn_Excluir.TabIndex = 15;
+            this.btn_Excluir.Text = "Excluir";
+            this.btn_Excluir.UseVisualStyleBackColor = true;
+            this.btn_Excluir.Click += new System.EventHandler(this.btn_Excluir_Click);
+            // 
+            // btn_Editar
+            // 
+            this.btn_Editar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Editar.Location = new System.Drawing.Point(340, 294);
+            this.btn_Editar.Name = "btn_Editar";
+            this.btn_Editar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Editar.TabIndex = 16;
+            this.btn_Editar.Text = "Editar";
+            this.btn_Editar.UseVisualStyleBackColor = true;
+            this.btn_Editar.Click += new System.EventHandler(this.btn_Editar_Click);
+            // 
+            // label7
+            // 
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(67, 29);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(379, 23);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Cadastro Cliente";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 337);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btn_Editar);
+            this.Controls.Add(this.btn_Excluir);
+            this.Controls.Add(this.btn_pesquisar);
+            this.Controls.Add(this.txt_Endereco);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_telefone);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_Email);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_sobrenome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.txt_Nome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cadastro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Nome;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_sobrenome;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Email;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker txt_dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox txt_telefone;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_Endereco;
+        private System.Windows.Forms.Button btn_pesquisar;
+        private System.Windows.Forms.Button btn_Excluir;
+        private System.Windows.Forms.Button btn_Editar;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
